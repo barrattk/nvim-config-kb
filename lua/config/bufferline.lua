c41726a82fd7ca3df0c1b4a0e3f06bcc4e48fb7c
Keith Barratt <k.barratt@systemloco.com>
2025-04-08 09:38:54 +0100

    Key mappings for bufferline

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -31,17 +31,32 @@ M.config = function ()
         }
     }
 
-    map('n', '<A-1>', '<CMD>BufferLineGoToBuffer 1<CR>', opts)
-    map('n', '<A-2>', '<CMD>BufferLineGoToBuffer 2<CR>', opts)
-    map('n', '<A-3>', '<CMD>BufferLineGoToBuffer 3<CR>', opts)
-    map('n', '<A-4>', '<CMD>BufferLineGoToBuffer 4<CR>', opts)
-    map('n', '<A-5>', '<CMD>BufferLineGoToBuffer 5<CR>', opts)
-    map('n', '<A-6>', '<CMD>BufferLineGoToBuffer 6<CR>', opts)
-    map('n', '<A-7>', '<CMD>BufferLineGoToBuffer 7<CR>', opts)
-    map('n', '<A-8>', '<CMD>BufferLineGoToBuffer 8<CR>', opts)
-    map('n', '<A-9>', '<CMD>BufferLineGoToBuffer 9<CR>', opts)
-    -- Last visable buffer
-    map('n', '<A-0>', '<Cmd>BufferLineGoToBuffer -1<CR>', opts)
+    -- For absolute positions we can do the following
+    vim.keymap.set('n', '<A-1>', ':lua require("bufferline").go_to_buffer(1, true)<CR>', { silent = true })
+    vim.keymap.set('n', '<A-2>', ':lua require("bufferline").go_to_buffer(2, true)<CR>', { silent = true })
+    vim.keymap.set('n', '<A-3>', ':lua require("bufferline").go_to_buffer(3, true)<CR>', { silent = true })
+    vim.keymap.set('n', '<A-4>', ':lua require("bufferline").go_to_buffer(4, true)<CR>', { silent = true })
+    vim.keymap.set('n', '<A-5>', ':lua require("bufferline").go_to_buffer(5, true)<CR>', { silent = true })
+    vim.keymap.set('n', '<A-6>', ':lua require("bufferline").go_to_buffer(6, true)<CR>', { silent = true })
+    vim.keymap.set('n', '<A-7>', ':lua require("bufferline").go_to_buffer(7, true)<CR>', { silent = true })
+    vim.keymap.set('n', '<A-8>', ':lua require("bufferline").go_to_buffer(8, true)<CR>', { silent = true })
+    vim.keymap.set('n', '<A-9>', ':lua require("bufferline").go_to_buffer(9, true)<CR>', { silent = true })
+    -- Last visible buffer
+    vim.keymap.set('n', '<A-0>', ':lua require("bufferline").go_to_buffer(-1, true)<CR>', { silent = true })
+
+
+    -- This seemed to go off the buffer-id which I didn't like?
+    -- map('n', '<A-1>', '<CMD>BufferLineGoToBuffer 1<CR>', opts)
+    -- map('n', '<A-2>', '<CMD>BufferLineGoToBuffer 2<CR>', opts)
+    -- map('n', '<A-3>', '<CMD>BufferLineGoToBuffer 3<CR>', opts)
+    -- map('n', '<A-4>', '<CMD>BufferLineGoToBuffer 4<CR>', opts)
+    -- map('n', '<A-5>', '<CMD>BufferLineGoToBuffer 5<CR>', opts)
+    -- map('n', '<A-6>', '<CMD>BufferLineGoToBuffer 6<CR>', opts)
+    -- map('n', '<A-7>', '<CMD>BufferLineGoToBuffer 7<CR>', opts)
+    -- map('n', '<A-8>', '<CMD>BufferLineGoToBuffer 8<CR>', opts)
+    -- map('n', '<A-9>', '<CMD>BufferLineGoToBuffer 9<CR>', opts)
+    -- Last visible buffer
+    -- map('n', '<A-0>', '<Cmd>BufferLineGoToBuffer -1<CR>', opts)
 
     map('n', '<C-p>', '<CMD>BufferLinePick<CR>', opts)
 end
--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,9 +1,7 @@
 
   -- Fuzzy Finder (files, lsp, etc)
 local M = {
-
     'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       {
